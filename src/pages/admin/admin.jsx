04c95cee4cb7ed47cfa75ef91918290b131066f1
dwj <dwj@gmail.com>
2019-01-15 23:33:02 +0800

    主体内容路由加入

--- src/pages/admin/admin.jsx
+++ src/pages/admin/admin.jsx
@@ -6,6 +6,14 @@ import MemoryUtils from '../../utils/MemoryUtils'
 import LeftNav from '../../components/left-nav/left-nav'
 import Header from '../../components/header/header'
 import Footer from '../../components/footer/footer'
+import Home from '../home/home'
+import Category from '../category/category'
+import Product from '../product/product'
+import User from '../user/user'
+import Role from '../role/role'
+import Bar from '../charts/bar'
+import Line from '../charts/line'
+import Pie from '../charts/pie'
 /*
 后台管理主界面的路由组件
  */
@@ -26,7 +34,17 @@ export default class Admin extends Component {
         <Col span={20} className='main'>
           <Header/>
           <div className='content'>
-
+            <switch>
+              <Route path='/home' component={Home}/>
+              <Route path='/category' component={Category}/>
+              <Route path='/product' component={Product}/>
+              <Route path='/user' component={User}/>
+              <Route path='/role' component={Role}/>
+              <Route path='/charts/bar' component={Bar}/>
+              <Route path='/charts/line' component={Line}/>
+              <Route path='/charts/pie' component={Pie}/>
+              <Redirect to='/home'/>
+            </switch>
           </div>
           <Footer/>
         </Col>
