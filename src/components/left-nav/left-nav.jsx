50d212fbc46fd9cdc43889f4b2b7eb9e93c93e38
dwj <dwj@gmail.com>
2019-01-15 23:22:10 +0800

    left-nav

--- src/components/left-nav/left-nav.jsx
+++ src/components/left-nav/left-nav.jsx
@@ -1,9 +1,45 @@
 import React from 'react'
+import {Menu, Icon} from 'antd'
+import {NavLink, withRouter} from 'react-router-dom'
 
 import './left-nav.less'
 import logo from '../../assets/images/logo.png'
+import menuList from '../../config/menuConfig'
+
+const SubMenu = Menu.SubMenu
+const Item = Menu.Item
 
 export default class LeftNav extends React.Component {
+
+  //数组变标签数组
+  getNodes = (list) =>{
+    return list.reduce((pre,item) =>{
+      if(item.children){
+        const subMenu = (
+          <SubMenu key={item.path} title={<span><Icon type={item.icon} /><span>{item.title}</span></span>}>
+            {
+              this.getNodes(item.children)
+            }
+          </SubMenu>
+        )
+        pre.push(subMenu)
+      }else {
+        const menuItem = (
+          <Item key={item.key}>
+            <NavLink to={item.key}>
+              <Icon type={item.icon}/> {item.title}
+            </NavLink>
+          </Item>
+        )
+        pre.push(menuItem)
+      }
+      return pre
+    },[])
+  }
+
+  componentWillMount() {
+    this.menuNodes = this.getNodes(menuList)
+  }
 
   render() {
 
@@ -13,6 +49,11 @@ export default class LeftNav extends React.Component {
           <img src={logo} alt="logo"/>
           <h1>硅谷后台</h1>
         </div>
+        <Menu mode="inline" theme='dark'>
+          {
+            this.menuNodes
+          }
+        </Menu>
       </div>
     )
   }
