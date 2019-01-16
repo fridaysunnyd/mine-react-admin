705b7a78e74fecc75c5ab5dfc1e7d74f6a5dba81
dwj <dwj@gmail.com>
2019-01-16 14:31:54 +0800

    header组件完成

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,28 @@
 //ajax(url, data={}, method='GET')
 import ajax from './ajax'
+import jsonp from 'jsonp'
 
+//登录
 export const reqLogin = (username,password) => ajax('/login',{username,password},'POST')
-
+//请求天气
+export function reqWeather(city) {
+  return new Promise((res,rej) => {
+    const url = `http://api.map.baidu.com/telematics/v3/weather?location=${city}&output=json&ak=3p49MVra6urFRGOT9s8UBWr2`
+    jsonp(
+      url,
+      {
+        param: 'callback'
+      },
+      (error,data) =>{
+        if(!error) {
+          const {dayPictureUrl, temperature} = data.results[0].weather_data[0]
+          res({dayPictureUrl, temperature})
+        } else {
+          alert('请求天气接口出错啦!!!')
+        }
+      }
+    )
+  })
+}
 
 
--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,15 +1,101 @@
 import React from 'react'
+import {Row,Col,Modal} from 'antd'
+import {withRouter} from 'react-router-dom'
 
 import './header.less'
+import memoryUtiles from '../../utils/MemoryUtils'
+import storageUtils from '../../utils/storageUtils'
+import menuList from '../../config/menuConfig'
+import {formateDate} from '../../utils'
+import {reqWeather} from '../../api'
 
-export default class Header extends React.Component {
+ class Header extends React.Component {
+    state = {
+      sysTime:formateDate(Date.now()),
+      dayPictureUrl:'',
+      temperature:''
+    }
+
+     getMenuName = (path) => {
+       let menuName
+       menuList.forEach(menuItem => {
+         if (menuItem.key === path){
+           menuName = menuItem.title
+         }else if(menuItem.children){
+           menuItem.children.forEach(
+             menuItem => {
+               if (menuItem.key === path) {
+                 menuName = menuItem.title
+               }
+             })
+         }
+       })
+       return menuName || '首页'
+
+     }
+    logout = () => {
+      Modal.confirm({
+        content: '确定退出吗?',
+        onOk: () => {
+          storageUtils.removeUser()
+          memoryUtiles.user = {}
+          this.props.history.replace('/login')
+        }
+      })
+    }
+     getSysTime = () =>{
+      this.intervalId = setInterval(() =>{
+        this.setState({
+          sysTime:formateDate(Date.now())
+        })
+      },1000)
+     }
+    getWeather = async() =>{
+      const {dayPictureUrl, temperature} = await reqWeather('北京')
+      this.setState({
+        dayPictureUrl,
+        temperature
+      })
+    }
+    componentDidMount(){
+      this.getSysTime()
+      this.getWeather()
+    }
+    componentWillUnmount(){
+      clearInterval(this.intervalId)
+    }
 
   render() {
-
+    const  {username} = memoryUtiles.user
+    const path = this.props.location.pathname
+    const menuName = this.getMenuName(path)
+    const {sysTime,dayPictureUrl,temperature} = this.state
     return (
-      <div className='header'>
-        Header
+      <div className="header">
+        <Row className="header-top">
+          <Col span={24}>
+            <span>欢迎，{username}</span>
+            <a href="javascript:" onClick={this.logout}>退出</a>
+          </Col>
+        </Row>
+        <Row className="breadcrumb">
+          <Col span={4} className="breadcrumb-title">
+            {menuName}
+          </Col>
+          <Col span={20} className="weather">
+            <span className="date">
+              {sysTime}
+            </span>
+            <span className="weather-img">
+                <img src={dayPictureUrl} alt=""/>
+            </span>
+            <span className="weather-detail">
+              {temperature}
+            </span>
+          </Col>
+        </Row>
       </div>
     )
   }
 }
+export default withRouter(Header)
