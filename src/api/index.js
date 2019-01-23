4cb99c66396809937100690d6044721e81676615
dwj <dwj@gmail.com>
2019-01-23 16:22:29 +0800

    用户管理完成

--- src/api/index.js
+++ src/api/index.js
@@ -2,41 +2,49 @@
 import ajax from './ajax'
 import jsonp from 'jsonp'
 
-//登录
+// 登录
 export const reqLogin = (username,password) => ajax('/login',{username,password},'POST')
 
-//根据分类ID获取分类
+// 根据分类ID获取分类
 export const reqCategoryName = (categoryId) => ajax('/manage/category/info',{categoryId})
-//请求分类列表
+// 请求分类列表
 export const reqCategory = (parentId) => ajax('/manage/category/list',{parentId})
-//添加分类
+// 添加分类
 export const reqAddCategory = ({parentId,categoryName}) => ajax('/manage/category/add',{parentId,categoryName},'POST')
-//更新品类名称
+// 更新品类名称
 export const reqUpdateCategory = ({categoryId,categoryName}) => ajax('/manage/category/update',{categoryId,categoryName},'POST')
 // updateCategory({categoryId:'5c3f04260ec9e53678305b10',categoryName:'ccc'}).then(data =>{console.log(data);})
 
 
-//获取商品分页列表
+// 获取商品分页列表
 export const reqProducts = (pageNum, pageSize) => ajax('/manage/product/list', {pageNum, pageSize})
-//搜索获取商品分页列表
+// 搜索获取商品分页列表
 export const reqSearchProducts = ({pageNum, pageSize,searchName,searchType}) => ajax('/manage/product/search',{
   [searchType]:searchName,
   pageNum,
   pageSize
 })
-//删除上传图片
+// 删除上传图片
 export const reqRemoveImg = (name) => ajax('/manage/img/delete',{name},'POST')
 // 添加或者更新商品
 export const reqAddUpdateProduct = (product) => ajax('/manage/product/' + (product._id ? 'update' : 'add'), product, 'POST')
-//上下架商品
+// 上下架商品
 export const reqUpdateProductState = (productId,status) => ajax('/manage/product/updateStatus',{productId,status},'POST')
 
-//获取角色列表
+// 获取角色列表
 export const reqRoles = () => ajax('/manage/role/list')
-//添加角色
+// 添加角色
 export const reqAddRole = (roleName) => ajax('/manage/role/add',{roleName},'POST')
 // 更新角色(给角色设置权限)
 export const reqUpdateRole = (role) => ajax('/manage/role/update', role, 'POST')
+
+// 获取用户列表
+export const reqUsers = () => ajax('/manage/user/list')
+// 添加/更新用户
+export const reqAddOrUpdateUser = (user) => ajax(user._id?'/manage/user/update':'/manage/user/add',user,'POST')
+// 删除用户
+export const reqDeleteUser = (userId) => ajax('/manage/user/delete',{userId},'POST')
+
 
 //请求天气
 export function reqWeather(city) {
--- src/pages/user/user.jsx
+++ src/pages/user/user.jsx
@@ -1,14 +1,229 @@
 import React, {Component} from 'react'
+import {
+  Card,
+  Button,
+  Table,
+  Form,
+  Input,
+  Select,
+  Modal,
+} from 'antd'
+
+import {reqUsers,reqAddOrUpdateUser,reqDeleteUser} from '../../api'
+import {formateDate} from '../../utils'
+
+const FormItem = Form.Item
+const Option = Select.Option
 
 /*
 后台管理的用户管理路由组件
  */
 export default class User extends Component {
+
+  state = {
+    users:[],
+    user:{},
+    isShow:false
+  }
+
+  initColumns = () => {
+    this.columns = [
+      {
+        title: '用户名',
+        dataIndex: 'username'
+      },
+      {
+        title: '邮箱',
+        dataIndex: 'email'
+      },
+      {
+        title: '电话',
+        dataIndex: 'phone'
+      },
+      {
+        title: '注册时间',
+        dataIndex: 'create_time',
+        render: formateDate
+      },
+      {
+        title: '所属角色',
+        dataIndex: 'role_id',
+        render: (role_id) => this.roleName[role_id]
+      },
+      {
+        title: '操作',
+        render: (user) => (
+          <span>
+            <a href="javascript:;" onClick={() => this.showUpdate(user)}>修改</a>
+            &nbsp;&nbsp;
+            <a href="javascript:;" onClick={() => this.clickDelete(user)}>删除</a>
+          </span>
+        )
+      },
+    ]
+  }
+  showUpdate = (user) =>{
+    this.setState({
+      user:user,
+      isShow:true
+    })
+  }
+  showAddUser = () =>{
+    this.setState({
+      user:{},
+      isShow:true
+    })
+
+  }
+  getUsers = async () =>{
+    const result = await reqUsers()
+    if(result.status === 0) {
+      const {users,roles} = result.data
+      this.roles = roles
+      this.roleName = roles.reduce((pre,role) =>{
+        const {_id,name} = role
+        pre[_id] = name
+        return pre
+      },{} )
+      this.setState({
+        users
+      })
+    }
+  }
+  AddOrUpdateUser =async () =>{
+    const user =  this.form.getFieldsValue()
+    this.form.resetFields()
+    if(this.state.user) {
+      user._id = this.state.user._id
+    }
+    this.setState({
+      isShow: false
+    })
+    const result = await reqAddOrUpdateUser(user)
+    if(result.status === 0){
+      this.getUsers()
+    }
+  }
+  clickDelete =async (user) =>{
+    const result = await reqDeleteUser(user._id)
+    if(result.status === 0){
+      this.getUsers()
+    }
+  }
+
+  componentWillMount(){
+    this.initColumns()
+  }
+  componentDidMount () {
+    this.getUsers()
+  }
+
   render() {
+    const {users,user,isShow} = this.state
     return (
       <div>
-        User
+        <Card>
+          <Button type="primary" onClick={this.showAddUser}>创建用户</Button>
+        </Card>
+
+        <Table
+          columns={this.columns}
+          rowKey='_id'
+          dataSource={users}
+          bordered
+          pagination={{defaultPageSize: 10, showQuickJumper: true}}
+        />
+
+        <Modal
+          title={user._id ? '修改用户' : '添加用户'}
+          visible={isShow}
+          onCancel={() => this.setState({isShow: false})}
+          onOk={this.AddOrUpdateUser}
+        >
+          <UserForm setForm={(form) => this.form = form} user={user} roles={this.roles} />
+        </Modal>
       </div>
     )
   }
 }
+
+class UserForm extends React.Component{
+
+  componentWillMount(){
+    this.props.setForm(this.props.form)
+  }
+
+  render (){
+    const {getFieldDecorator} = this.props.form
+    const formItemLayout = {
+      labelCol: {span: 5},
+      wrapperCol: {span: 16}
+    }
+    const {user,roles} = this.props
+    return (
+      <Form>
+        <FormItem label="用户名" {...formItemLayout}>
+          {
+            getFieldDecorator('username', {
+              initialValue: user.username
+            })(
+              <Input type="text" placeholder="请输入用户名"/>
+            )
+          }
+        </FormItem>
+
+        {
+          !user._id ?
+            (
+              <FormItem label="密码" {...formItemLayout}>
+                {
+                  getFieldDecorator('password', {
+                    initialValue: ''
+                  })(
+                    <Input type="passowrd" placeholder="请输入密码"/>
+                  )
+                }
+              </FormItem>
+            ) : null
+        }
+
+
+
+        <FormItem label="手机号" {...formItemLayout}>
+          {
+            getFieldDecorator('phone', {
+              initialValue: user.phone
+            })(
+              <Input type="phone" placeholder="请输入手机号"/>
+            )
+          }
+        </FormItem>
+
+        <FormItem label="邮箱" {...formItemLayout}>
+          {
+            getFieldDecorator('email', {
+              initialValue: user.email
+            })(
+              <Input type="email" placeholder="请输入邮箱"/>
+            )
+          }
+        </FormItem>
+
+        <FormItem label="角色" {...formItemLayout}>
+          {
+            getFieldDecorator('role_id', {
+              initialValue: user.role_id
+            })(
+              <Select style={{width: 200}}>
+                {
+                  roles.map(role => <Option key={role._id} value={role._id}>{role.name}</Option>)
+                }
+              </Select>
+            )
+          }
+        </FormItem>
+      </Form>
+    )
+  }
+}
+UserForm = Form.create()(UserForm)
